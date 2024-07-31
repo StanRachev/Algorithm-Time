7ff5e75e5a10795b78eab9c5e844de25f04eb4b1
Stanimir Rachev <rachev.stanimir7@gmail.com>
2024-07-31 11:20:22 +0300

    while relocating had to rename several methods

--- com/stanimir/AlgorithmTime/Main.java
+++ com/stanimir/AlgorithmTime/Main.java
@@ -1,5 +1,7 @@
 package stanimir.AlgorithmTime;
 
+import stanimir.AlgorithmTime.Util.SortManager;
+
 public class Main {
     public static void main(String[] args) {
         SortManager manager = new SortManager();
