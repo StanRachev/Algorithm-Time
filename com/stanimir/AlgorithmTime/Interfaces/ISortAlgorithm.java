48c0ccefd290723a4c26005c17fe9241e064a964
Stanimir Rachev <rachev.stanimir7@gmail.com>
2024-07-31 11:06:12 +0300

    implementing functional interfaces for method reference

---
+++ com/stanimir/AlgorithmTime/Interfaces/ISortAlgorithm.java
@@ -0,0 +1,6 @@
+package stanimir.AlgorithmTime.Interfaces;
+
+@FunctionalInterface
+public interface ISortAlgorithm {
+    void sort(int[] array);
+}
---
+++ com/stanimir/AlgorithmTime/Interfaces/ISortAlgorithmStartEnd.java
@@ -0,0 +1,6 @@
+package stanimir.AlgorithmTime.Interfaces;
+
+@FunctionalInterface
+public interface ISortAlgorithmStartEnd {
+    void sort(int[] array, int start, int end);
+}
